2a1583d23eeb25c8ca1c07867ee4368aac7303a5
KASHIMA-Takeru <md23036@shibaura-it.ac.jp>
2024-08-19 14:35:18 +0900

    add osnet.py
    added python file of osnet

--- processors/reid/osnet.py
+++ processors/reid/osnet.py
@@ -5,7 +5,7 @@ from torch import nn
 from torch.nn import functional as F
 
 __all__ = [
-    'osnet_x1_0', 'osnet_x0_75', 'osnet_x0_5', 'osnet_x0_25', 'osnet_ibn_x1_0'
+    'osnet_x1_0'
 ]
 
 pretrained_urls = {
