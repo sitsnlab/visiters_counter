562e09856f2aee65fd1c85e10657149091dbd905
Y.Kuronuma <78721329+Oni-Tenzikunezumi@users.noreply.github.com>
2024-08-25 17:14:15 +0900

    Update data_IO.py
    グラフ表示機能を時間単位に変更
    タイトルに男女別の計測結果を追加

--- processors/data_IO.py
+++ processors/data_IO.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""検出結果をエクセルに書き出す.
+"""検出結果のCSV書き出し，読み込み.
 
 """
 
@@ -10,6 +10,7 @@ import tqdm
 import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import MaxNLocator
 
 HEADER = ['visited_numb', "person_id", "time", "gender", "age"]
 
@@ -18,10 +19,23 @@ class DataWriter:
     """検出結果をCSVに書き出すクラス."""
 
     def __init__(self, save_path: str = None):
+        """イニシャライザ.
+
+
+        Parameters
+        ----------
+        save_path : str, optional
+            保存先のファイル. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
 
         if save_path is None:
             now = dt.datetime.now()
-            save_path = now.strftime('%H_%M_%S_%f') + '.csv'
+            save_path = now.strftime('%Y_%m%d_%H%M') + '.csv'
             self.save_file = plib('data') / save_path
 
             with open(self.save_file, 'w') as f:
@@ -29,10 +43,24 @@ class DataWriter:
         else:
             self.save_file = plib(save_path)
 
-    def weite_file(self, text):
+        self.write_time()
+
+    def write_file(self, text):
         """データを書き込む."""
         with open(self.save_file, 'a') as f:
             f.write(','.join(text) + '\n')
+
+    def write_time(self):
+        """データの時間をつける."""
+        data = {}
+
+        data['visited_numb'] = '-1'
+        data['person_id'] = "None"
+        data['time'] = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        data['gender'] = 'None'
+        data['age'] = '-1'
+
+        self.write_file(data.values())
 
 
 class DataReader:
@@ -43,6 +71,7 @@ class DataReader:
         self.file_path = plib(file_path)
         self.dframe = pd.read_csv(self.file_path, header=0, names=HEADER)
         self.categorize_data()
+        self.make_title()
 
     def categorize_data(self):
         male = self.dframe[self.dframe['gender'] == 'male']
@@ -52,25 +81,41 @@ class DataReader:
         self.female_plot = self.time2label(female)
 
     def time2label(self, gender):
-        # '%H:%M:%S.%f'
         # '%Y-%m-%d %H:%M:%S.%f'
         time_rabel = []
         for data in gender['time']:
+            data = data.split(sep=' ')[1]
             h, m, s = list(map(float, data.split(sep=':')))
-            # print(h, m, s, h + m/60)
-            time_rabel.append(h + m/60)
+            # time_rabel.append(h + m/60)
+            time_rabel.append(h)
 
         return time_rabel
 
+    def make_title(self):
+        date, time = self.dframe['time'][0].split(sep=' ')
+        male = len(self.dframe[self.dframe['gender'] == 'male'])
+        female = len(self.dframe[self.dframe['gender'] == 'female'])
+
+        times = self.dframe['time'].to_list()
+        self.time_s = int(times[0].split(sep=' ')[1].split(sep=':')[0])
+        self.time_e = int(times[-1].split(sep=' ')[1].split(sep=':')[0])
+
+        form = 'Visitor of {}, time:{}~{}\n'.format(date, self.time_s, self.time_e)
+        form += 'Male:{}, Female:{}'.format(male, female)
+        self.title = form.format(date, male, female)
 
     def show_plot(self):
-        # rng = np.random.default_rng()
-        # x = rng.standard_normal(1000)
-        # y = rng.standard_normal(1000)
+        bins = self.time_e - self.time_s + 1
 
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.hist([self.male_plot, self.female_plot], stacked=False)
+        ax.hist([self.male_plot, self.female_plot],
+                stacked=False, range=(self.time_s, self.time_e), bins=bins)
+
+        fig.suptitle(self.title)
+        ax.legend(["Male", "Female"])
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.grid(True)
 
         plt.show()
 
@@ -86,9 +131,12 @@ if __name__ == '__main__':
     # dw.weite_file(a)
     # print(','.join('sasakama'))
 
-    path = r'..\data\output13_58_02_658691.csv'
+    path = r'..\data\2024_0823_0349.csv'
+    path = r'..\data\2024_0823_0349.csv'
+
+    path = r'..\data\2024_0823_0349.csv'
+    path = r'..\data\2024_0825_pm.csv'
+
     dr = DataReader(path)
     dr.categorize_data()
-    print(dr.dframe)
-    # print(dr.male_plot)
     dr.show_plot()
